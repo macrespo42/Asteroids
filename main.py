1aa4eac5a95c93b9cc5107865cf38e424ab3912c
macrespo42 <macrespo@student.42.fr>
2024-08-22 21:59:20 +0200

    feat: init game loop and quit game properly

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pygame
 
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 
 def main():
+    numpass, numfail = pygame.init()
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("#000000")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
